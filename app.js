b7a9709a9d7771d394ab3b155117721e7776e58e
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-07-13 16:21:05 +0200

    arrow functions and more

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-console.log("It works!");
+var variable = "Value";
+console.log(variable);
+variable = "Another value";
+console.log(variable);
+//Block scope
+function reset() {
+    var variable = null;
+    console.log(variable);
+}
+reset();
+console.log(variable);
+//Arrow dunctions
+console.log("Arrow function");
+var addNumbers = function (number1, number2) {
+    return number1 + number2;
+};
+console.log(addNumbers(10, 3));
+var multiplyNumbers = function (number1, number2) {
+    return number1 * number2;
+};
+console.log(multiplyNumbers(10, 3));
+var greetings = function () { return "Greetings "; };
+console.log(greetings());
+var greetFriend = function (friend) { return console.log(greetings() + friend); };
+greetFriend("Jarek");
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -1,1 +1,31 @@
-console.log("It works!"); 
+
+let variable = "Value";
+console.log(variable); 
+variable =  "Another value"
+console.log(variable); 
+
+//Block scope
+
+function reset() {
+    let variable = null;
+    console.log(variable)
+}
+reset();
+console.log(variable)
+
+//Arrow dunctions
+console.log("Arrow function");
+const addNumbers = function(number1: number, number2: number): number{
+    return number1 + number2
+};
+console.log(addNumbers(10,3));
+
+const multiplyNumbers = (number1: number, number2: number) => {
+    return number1 * number2 };
+    console.log(multiplyNumbers(10,3));
+
+const greetings = () => "Greetings ";
+console.log(greetings());
+
+const greetFriend = friend => console.log(greetings() + friend);
+greetFriend("Jarek");

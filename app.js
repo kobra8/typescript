c843fe9a7c14431755cb50324cb16c5036b699d9
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-07-26 17:30:32 +0200

    Destructing arrays and objects

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ function reset() {
 }
 reset();
 console.log(variable);
-//Arrow dunctions
+//Arrow functions
 console.log("Arrow function");
 var addNumbers = function (number1, number2) {
     return number1 + number2;
@@ -23,4 +23,22 @@ var greetings = function () { return "Greetings "; };
 console.log(greetings());
 var greetFriend = function (friend) { return console.log(greetings() + friend); };
 greetFriend("Jarek");
+console.log("SPREAD & REST");
+var numbers = [1, 23, 45, 67, 87, 99];
+console.log(Math.max.apply(Math, numbers)); // SPREAD change array into chain of values -> spread sign ...
+function makeArray() {
+    var args = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        args[_i] = arguments[_i];
+    }
+    return args;
+}
+console.log(makeArray(1, 2, 3, 6));
+console.log("Destructing arrays and objects:");
+var hobbies = ["Railways", "Mountains"];
+var hobby1 = hobbies[0], hobby2 = hobbies[1];
+console.log("Hob1:" + hobby1, "Hob2:" + hobby2);
+var myPerson = { myName: "Jerzy", age: 42 };
+var myName = myPerson.myName, age = myPerson.age;
+console.log(myName, age);
 //# sourceMappingURL=app.js.map
--- app.ts
+++ app.ts
@@ -29,3 +29,21 @@ console.log(greetings());
 
 const greetFriend = friend => console.log(greetings() + friend);
 greetFriend("Jarek");
+
+console.log("SPREAD & REST");
+const numbers = [1, 23, 45, 67, 87, 99];
+console.log(Math.max(...numbers)); // SPREAD change array into chain of values -> spread sign ...
+
+function makeArray(...args: number[]) { // REST change values into array -> rest sign is ... -> it works like rest when use in functions
+    return args;
+}
+console.log(makeArray(1, 2, 3, 6))
+
+console.log("Destructing arrays and objects:");
+const hobbies = ["Railways", "Mountains"];
+const [hobby1, hobby2] = hobbies;
+console.log("Hob1:" + hobby1, "Hob2:" + hobby2);
+
+const myPerson = {myName: "Jerzy", age: 42}
+const {myName, age} = myPerson;
+console.log(myName, age);

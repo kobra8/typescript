d46e5bf17bd3e561025430fae4c3addfbf0eca56
Jerzy Drobot <jerzy.drobot@mggp.com.pl>
2017-09-22 14:51:18 +0200

    Course finished

---
+++ react_ts/src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { Home } from "./components/home";
+
+ReactDOM.render(<Home name="Jerzy" age={42}/>, document.getElementById("app"));
